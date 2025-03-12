6b0111d40d636081506a111b717a0f45fee78496
egor <egor@ivra.ru>
2025-03-12 12:32:56 +0300

    Init

--- run.sh
+++ run.sh
@@ -2,28 +2,48 @@
 
 source .env
 
-if [[ -z "$REDIS_PASSWORD" || -z "$REDIS_EXTERNAL_PORT" ]]; then
-  echo "Error: Variables REDIS_PASSWORD or REDIS_EXTERNAL_PORT or REDIS_PORT are not set in .env file."
+if [[ -z "$REDIS_PASSWORD" || -z "$REDIS_EXTERNAL_PORT" || -z "$REDIS_PORT" ]]; then
+  echo "Error: Variables REDIS_PASSWORD, REDIS_EXTERNAL_PORT, or REDIS_PORT are not set in .env file."
   exit 1
 fi
 
-redis_container="redis"
+redis_container="redis_server"
 
 containers=(
-  "redis"
+  "redis_server"
 )
+
+echo -e "\n==================================="
 
 echo "Stopping specified containers..."
 for container in "${containers[@]}"; do
   docker stop "$container"
 done
 
-echo "Rebuilding and starting containers..."
-docker compose up -d --build
+echo -e "==================================="
 
-echo "Checking Redis connection..."
-echo "PING?"
-docker compose exec "$redis_container" redis-cli -a "$REDIS_PASSWORD" --no-auth-warning ping
+docker compose up -d
+
+echo -e "==================================="
+
+echo "Checking Redis connection...  "
+
+PING_RESULT=$(docker compose exec "$redis_container" redis-cli -a "$REDIS_PASSWORD" --no-auth-warning ping 2>/dev/null)
+
+if [[ "$PING_RESULT" == "PONG" ]]; then
+  printf "\e[33mPING\e[0m  ------------------>  \e[32m%s\e[0m\n" "$PING_RESULT"
+else
+  printf "\e[33mPING\e[0m  ------------------>  \e[31mFailed\e[0m\n"
+
+  echo -e "==================================="
+  printf "\e[31m$PING_RESULT\e[0m\n"
+  echo -e "==================================="
+
+  echo -n "Stopped: "
+  docker stop "$redis_container"
+  echo -e "===================================\n"
+  exit 1
+fi
 
 #echo "Allowing external Redis port $REDIS_EXTERNAL_PORT in ufw..."
 #ufw allow "$REDIS_EXTERNAL_PORT"/tcp
@@ -32,7 +52,16 @@ docker compose exec "$redis_container" redis-cli -a "$REDIS_PASSWORD" --no-auth-
 #echo "Checking if the port $REDIS_EXTERNAL_PORT is open..."
 #ss -tlnp | grep "$REDIS_EXTERNAL_PORT"
 
-echo "Container IP:"
-docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' redis-redis-1
+echo -e "==================================="
 
-docker compose exec "$redis_container" redis-cli -a "$REDIS_PASSWORD" --no-auth-warning
+echo -n "Container IP: "
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$redis_container"
+
+echo -n "Host IP: "
+ip -4 addr show docker0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'
+
+echo "Port: $REDIS_EXTERNAL_PORT"
+
+echo -e "===================================\n"
+
+docker compose exec -it "$redis_container" redis-cli -a "$REDIS_PASSWORD" --no-auth-warning
