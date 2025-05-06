a3980cb60d5ccf6f9a7829e3632547ad59a57d5c
egor <egor@ivra.ru>
2025-05-06 16:03:39 +0300

    DN-581

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -36,8 +36,8 @@ echo -e "==================================="
 
 docker compose up -d --build
 
-docker cp "$REDIS_ACL" "$redis_container:etc/redis/users.acl"
-docker cp "$REDIS_CONF" "$redis_container:etc/redis/redis.conf"
+docker cp "$REDIS_ACL" "$redis_container:/etc/redis/users.acl"
+docker cp "$REDIS_CONF" "$redis_container:/etc/redis/redis.conf"
 docker restart "$redis_container"
 
 echo -e "==================================="
